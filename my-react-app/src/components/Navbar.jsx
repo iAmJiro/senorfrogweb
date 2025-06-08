84f242c31f7d8949a5fee29e8e578bcb3aff990e
iAmJiro <bryantcavinta24@gmail.com>
2025-06-08 22:41:54 +1200

    home page progress

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../App.css";
+function Navbar() {
+  return <div class="w-full h-screen bg-black"></div>;
+}
+export default Navbar;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -8,9 +8,11 @@ import {
 import { createRoot } from "react-dom/client";
 import Home from "../src/components/Home.jsx";
 import Products from "./components/Products.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Router>
+    {/* <Navbar></Navbar> */}
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
