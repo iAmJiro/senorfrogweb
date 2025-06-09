8cff2bf8d6e7590d513f18d11c5dfc8ece45cb40
iAmJiro <bryantcavinta24@gmail.com>
2025-06-09 14:13:49 +1200

    shirts added

--- my-react-app/src/components/GalleryHome.jsx
+++ my-react-app/src/components/GalleryHome.jsx
@@ -2,18 +2,18 @@ import React, { useRef } from "react";
 import { motion, useInView } from "framer-motion";
 
 const images = [
-  "crowd.jpg",
-  "public/431946070_18392579269078691_1116422623929486316_n.jpg",
-  "public/396314341_180883701743865_5099227960204058975_n.jpg",
-  "public/445359299_18404994271078691_1589283172037968775_n.jpg",
-  "public/447594800_18406132345078691_8101545124749734870_n.jpg",
-  "public/379586008_151535684678667_2712028302445206015_n.jpg",
-  "public/454743678_351398634692370_7735284383617932701_n.jpg",
-  "public/392885009_168487586316810_4619776564053201971_n.jpg",
-  "public/452551449_18414693244078691_6256823589942776058_n.jpg",
-  "public/449655951_18410921017078691_3306497815736442749_n.jpg",
-  "public/428027090_18384784174078691_6688384726961890193_n.jpg",
-  "public/370391783_138219832676919_470360915604256862_n.jpg",
+  "../public/images/shirtimages/shirt1-1.png",
+  "../public/images/shirtimages/shirt2-1.png",
+  "../public/images/shirtimages/shirt3-1.png",
+  "../public/images/shirtimages/shirt4-1.png",
+  "../public/images/shirtimages/shirt5-1.png",
+  "../public/images/shirtimages/shirt6-1.png",
+  "../public/images/shirtimages/shirt7-1.png",
+  "../public/images/shirtimages/shirt8-1.png",
+  "../public/images/shirtimages/shirt9-1.png",
+  "../public/images/shirtimages/shirt10-1.png",
+  "../public/images/shirtimages/shirt11-1.jpg",
+  "../public/images/shirtimages/shirt12-1.jpg",
 ];
 
 const GalleryHome = () => {
