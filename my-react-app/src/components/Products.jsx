f36edbfad9f20107a98f7feff8c8089728825e17
iAmJiro <bryantcavinta24@gmail.com>
2025-06-11 23:21:10 +1200

    Update Products.jsx

--- my-react-app/src/components/Products.jsx
+++ my-react-app/src/components/Products.jsx
@@ -556,7 +556,7 @@ const ProductShowcase = () => {
 
       {/* Product Details Modal */}
       {selectedProduct && modalImages && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-4 md:p-10">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-4 md:p-10 h-full">
           {showEmailStep ? (
             <div className="bg-white p-6 md:mt-16 md:p-8 rounded-lg shadow-lg w-full max-w-sm md:max-w-md lg:max-w-lg">
               <h2 className="text-xl md:text-2xl font-bold mb-4 text-center">
@@ -618,7 +618,7 @@ const ProductShowcase = () => {
             </div>
           ) : (
             <div
-              className={`bg-white p-6 mt-16 md:mt-0 md:p-8 rounded-lg shadow-lg h-full ${
+              className={`bg-white p-6 mt-16 md:mt-0 md:p-8 rounded-lg shadow-lg ${
                 isExpanded ? "max-w-5xl" : "max-w-4xl"
               } w-full flex flex-col md:flex-row`}
             >
